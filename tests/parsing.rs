9bf927310b29b370a70fc709b10c369bc40d86b6
Kurt Lawrence <kurtlawrence92@gmail.com>
2020-04-14 12:47:11 +1000

    named containers work

--- tests/parsing.rs
+++ tests/parsing.rs
@@ -130,9 +130,11 @@ mod tuples {
 
         do_test!("(<something> 123456789, 255)", &ans);
         do_test!("(    <something>  \t\t 123456789  \t,   255 \t\t )", &ans);
-        do_test!("A-Name (    <something>  \t\t 123456789  \t,   255 \t\t )", &ans);
+        do_test!(
+            "A-Name (    <something>  \t\t 123456789  \t,   255 \t\t )",
+            &ans
+        );
     }
-
 
     #[test]
     fn test_tuple_concise() {
@@ -243,6 +245,12 @@ mod containers {
                 map!("a" Kserd::new_unit(), "b" Kserd::new_num(2))
             ))
         );
+        do_test!(
+            "Some-Name(a   =  ()  \t, b \t = \t 2)",
+            &Kserd::new(Value::Cntr(
+                map!("a" Kserd::new_unit(), "b" Kserd::new_num(2))
+            ))
+        );
 
         let primitives = Primitives {
             a: (),
@@ -284,7 +292,7 @@ mod containers {
             wx: ByteArray(&[0, 1, 2, 5, 10, 20, 50, 100]),
         };
 
-        let kserd_ = as_kserd(&primitives);
+        let kserd = as_kserd(&primitives);
 
         let mut config = FormattingConfig {
             id_on_primitives: true,
@@ -293,12 +301,21 @@ mod containers {
         };
 
         config.width_limit = Some(80);
-        let s = kserd_.as_str_with_config(config);
-        do_test!(&s, &kserd_);
+        let s = kserd.as_str_with_config(config);
+        do_test!(&s, &kserd);
 
         config.width_limit = Some(60);
-        let s = kserd_.as_str_with_config(config);
-        do_test!(&s, &kserd_);
+        let s = kserd.as_str_with_config(config);
+        do_test!(&s, &kserd);
+
+        let s = "A-Name (
+        a = 1
+        b = 2
+        )";
+        do_test!(
+            &s,
+            &Kserd::new_cntr(vec![("a", Kserd::new_num(1)), ("b", Kserd::new_num(2))]).unwrap()
+        );
     }
 
     #[test]
