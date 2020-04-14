97bcc37c7f1fab94a6ec488dd31766abeec1007c
Kurt Lawrence <kurtlawrence92@gmail.com>
2020-04-14 12:52:05 +1000

    named maps passing

--- tests/parsing.rs
+++ tests/parsing.rs
@@ -673,6 +673,15 @@ mod map {
             ..Default::default()
         });
         do_test!(&s, &ans);
+
+        do_test!(
+            "A-Crazy-Map {0:1,2:3,4:5}",
+            &Kserd::new(Value::new_map(vec![
+                (n!(0), n!(1)),
+                (n!(2), n!(3)),
+                (n!(4), n!(5))
+            ]))
+        );
     }
 
     #[test]
